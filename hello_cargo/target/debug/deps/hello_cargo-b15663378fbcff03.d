fca3d95e00bd4868fd44c45062726ec0c1bb8c32
ColtWhitley <coltwhitley3@gmail.com>
2024-04-23 10:23:21 -0600

    Finished chapter 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
@@ -0,0 +1,5 @@
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
+
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/hello_cargo/target/debug/hello_cargo: /Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/hello_cargo/src/main.rs
