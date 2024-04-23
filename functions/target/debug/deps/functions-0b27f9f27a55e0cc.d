2dde3953f5c76ba34b886a49246d15500a0cfc86
ColtWhitley <coltwhitley3@gmail.com>
2024-04-23 12:56:31 -0600

    Finished 3.3 and 3.4 in the book

---
+++ functions/target/debug/deps/functions-0b27f9f27a55e0cc.d
@@ -0,0 +1,5 @@
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/functions/target/debug/deps/functions-0b27f9f27a55e0cc: src/main.rs
+
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/functions/target/debug/deps/functions-0b27f9f27a55e0cc.d: src/main.rs
+
+src/main.rs:
