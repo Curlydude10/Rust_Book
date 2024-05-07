92c6e1d1309c48e20504aff989dd0256952a1d85
ColtWhitley <coltwhitley3@gmail.com>
2024-05-07 10:17:37 -0600

    Chapter 13.1

---
+++ closures_and_iterators/target/debug/closures_and_iterators.d
@@ -0,0 +1,1 @@
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/closures_and_iterators/target/debug/closures_and_iterators: /Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/closures_and_iterators/src/main.rs
---
+++ closures_and_iterators/target/debug/deps/closures_and_iterators-d9a897cd95b8c454.d
@@ -0,0 +1,5 @@
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/closures_and_iterators/target/debug/deps/closures_and_iterators-d9a897cd95b8c454: src/main.rs
+
+/Users/coltwhitley/Documents/Personal_Projects/Software_Projects/Rust_Book/closures_and_iterators/target/debug/deps/closures_and_iterators-d9a897cd95b8c454.d: src/main.rs
+
+src/main.rs:
